c1d318c1fdf5e713c7e69fa22a3a919cd05eef0b
Сергей Ефимочкин <Sergeyanton2007@gmail.com>
2018-05-10 23:25:20 +0300

    Client side

---
+++ hw4/FS_Client/Network_utils.h
@@ -0,0 +1,11 @@
+//
+// Created by Сергей Ефимочкин on 10.05.2018.
+//
+
+#ifndef FILE_SYSTEM_NETWORK_UTILS_H
+#define FILE_SYSTEM_NETWORK_UTILS_H
+
+void send_answer(int sock, char* answer);
+char* get_answer(int sock);
+
+#endif //FILE_SYSTEM_NETWORK_UTILS_H
---
+++ hw4/FS_Client/main_client.c
@@ -0,0 +1,8 @@
+//
+// Created by Сергей Ефимочкин on 10.05.2018.
+//
+#include "Client.h"
+
+int main() {
+    Client_run();
+}
